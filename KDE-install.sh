473fda1238a2a8d341e75785eaa80eb751fe25b4
sunmh <sunmh@justsy.com>
2019-08-30 08:53:33 +0800

    init

---
+++ KDE-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd `dirname $0`; pwd
+echo '准备添加32位支持'
+sudo dpkg --add-architecture i386
+echo '添加成功，准备刷新apt缓存信息...'
+sudo apt update
+echo '准备加入KDE启动支持'
+sudo apt install gnome-settings-daemon
+echo '即将开始安装...'
+sudo dpkg -i *.deb
+echo '安装完成，正在自动安装依赖...'
+sudo apt install -f 
+echo '依赖安装完成正在配置环境'
+cp ./gsd-xsettings.sh $HOME/.config/autostart-scripts/gsd-xsettings.sh
+echo '执行完成'
+$HOME/.config/autostart-scripts/gsd-xsettings.sh
---
+++ README.md
@@ -0,0 +1,14 @@
+# 自用 deepin-wine-wechat 2.6.8.65
+
+> 参考https://github.com/wszqkzqk/deepin-wine-ubuntu
+
+> 在`Kubuntu 19.04`上测试, 支持双屏
+
+## 使用
+
+* 运行`./install.sh` 或者 `./KDE-install.sh` 即可
+
+## 我遇到的问题
+
+* 刚启动后输入中路是方块, 不要紧张, 可以切换下几个输入框后输入后, 会正常, 然后就一直正常输入
+* kde 下任务栏图标右键失效的问题, 有个解决办法是登录前关闭特效`Shift+Alt+F12`, 成功后再次切换, 然后就可以正常操作了, 就是图标区域背景变黑, 但不影响使用
